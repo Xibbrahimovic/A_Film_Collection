20848d7b6d86506151d2f06e2b565daf5944f110
Kongxib Vue <kongcvue@gmail.com>
2021-11-07 11:17:12 -0600

    added two inputs to form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,10 +1,43 @@
+import { useDispatch } from "react-redux";
+
+import { useState } from "react";
 
 function AddMovie(){
+    // const history = useHistory();
+    const dispatch = useDispatch();
 
+    let [newMovie, setNewMovie] = useState({
+    title: '',
+    poster:'',
+    description: '',
+    genre_id: ''
+});
+
+    const handleNameChange = (event, property) => {
+        console.log('event happened', event);
+        setNewMovie({...newMovie, [property]: event.target.value
+        })
+    }
+
+    const addNewMovie = event => {
+        event.preventDefault();
+        dispatch({type: 'ADD_MOVIE', payload: newMovie})
+    }
     return(
+        <form>
 
         <input
-        />
+        type="text"
+        placeholder="Title"
+        value={newMovie.title}
+        onChange={(event) => handleNameChange(event, 'title')}/>
+
+        <input
+        type="text"
+        placeholder="Description"
+        value={newMovie.description}
+        onChange={(event) => handleNameChange(event, 'description')}/>
+        </form>
 
     )
 
