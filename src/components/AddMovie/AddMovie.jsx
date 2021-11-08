cf499c0da6219d994a8bb7f2e59ff2aa6f88be5f
Kongxib Vue <kongcvue@gmail.com>
2021-11-07 21:11:31 -0600

    was not able to get mui implemented with movie form and genres to be stored

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,74 +3,104 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useHistory } from "react-router";
 
-function AddMovie(){
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const genres = useSelector(store => store.genres);
+import { TextField } from "@mui/material";
+import Select from "@mui/material/Select";
+import MenuItem from '@mui/material/MenuItem';
+import { InputLabel } from "@mui/material";
+import { Button } from "@mui/material";
 
-    let [newMovie, setNewMovie] = useState({
-    title: '',
-    poster:'',
-    description: '',
-    genre_id: ''
-});
-    useEffect(() => {
-    dispatch({ type: 'FETCH_GENRES' });
-    }, []);
 
-    const handleNameChange = (event, property) => {
-        console.log('event happened', event);
-        setNewMovie({...newMovie, [property]: event.target.value
-        })
-    }
+function AddMovie() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const genres = useSelector((store) => store.genres);
 
-    const addNewMovie = event => {
-        event.preventDefault();
-        dispatch({type: 'ADD_MOVIE', payload: newMovie})
-        console.log('clicked! added new movie');
-    }
-    
-    console.log(genres);
-    return(
-        <form onSubmit={addNewMovie}>
-        <div>
-        <input
-        type="text"
-        placeholder="Title"
-        value={newMovie.title}
-        onChange={(event) => handleNameChange(event, 'title')}/>
-        </div>
-        <br>
-        </br>
-        <div>
-        <input
-        type="text"
-        placeholder="Poster (IMG URL)"
-        value={newMovie.poster}
-        onChange={(event) => handleNameChange(event, 'poster')}/>
-        </div>
+  let [newMovie, setNewMovie] = useState({
+    title: "",
+    poster: "",
+    description: "",
+    genre_id: "",
+  });
+  useEffect(() => {
+    dispatch({ type: "FETCH_GENRES" });
+  }, []);
 
-        <br>
-        </br>
+  const handleNameChange = (event, property) => {
+    console.log("event happened", event);
+    setNewMovie({ ...newMovie, [property]: event.target.value });
+  };
 
-        <div>
-        <input
-        type="text"
-        placeholder="Description"
-        value={newMovie.description}
-        onChange={(event) => handleNameChange(event, 'description')}/>
-        </div>
+  const addNewMovie = (event) => {
+    event.preventDefault();
+    dispatch({ type: "ADD_MOVIE", payload: newMovie });
+    console.log("clicked! added new movie");
+  };
 
-        <br>
-        </br>
+  console.log(genres);
+  return (
+    <form onSubmit={addNewMovie}>
+      <div>
+        <TextField
+          className="textField"
+          id="outlined-basic"
+          label="Title"
+          variant="outlined"
+          type="text"
+          placeholder="Title"
+          value={newMovie.title}
+          onChange={(event) => handleNameChange(event, "title")}
+        />
+      </div>
+      <br></br>
+      <div>
+        <TextField
+          className="textField"
+          id="outlined-basic"
+          label="Poster (IMG URL)"
+          variant="outlined"
+          type="text"
+          placeholder="Poster (IMG URL)"
+          value={newMovie.poster}
+          onChange={(event) => handleNameChange(event, "poster")}
+        />
+      </div>
 
-        <select 
+      <br></br>
+
+      <div>
+        <TextField
+          className="textField"
+          id="outlined-basic"
+          label="Description"
+          variant="outlined"
+          type="text"
+          placeholder="Description"
+          value={newMovie.description}
+          onChange={(event) => handleNameChange(event, "description")}
+        />
+      </div>
+
+      <br></br>
+    <InputLabel id="demo-simple-select-label">Genre</InputLabel>
+    <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={newMovie.genre_id}
+        label="Genre"
+        onChange={(event) => handleNameChange(event, 'genre_id')}
+        >
+        {genres.map((genre) => {
+            return(
+                <MenuItem key={genre.id} value={genre.id}>
+                    {genre.name}
+                </MenuItem>
+            )})}
+    </Select>
+      {/* <select 
         value={newMovie.genre_id}
         onChange={(event) => handleNameChange(event, 'genre_id')}>
-
         <br>
         </br>
-        
         <option disabled value ='0'>
             Genres
         </option>
@@ -81,27 +111,15 @@ function AddMovie(){
                 </option>
             )
         })}
+        </select> */}
+      <div>
+      <Button type="submit">Add New Movie</Button>
+        <br></br>
 
-        </select>
-        <button type="submit">Add New Movie</button>
-        <div>
-
-        <br>
-        </br>
-        
-        <button onClick={() => {history.push('/')}}>CANCEL</button>
-        </div>
-        </form>
-
-        
-
-        
-
-    )
-    
-
+        <button onClick={() => {history.push("/");}}>CANCEL</button>
+      </div>
+    </form>
+  );
 }
-
-
 
 export default AddMovie;
